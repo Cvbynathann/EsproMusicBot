6a011f985afb0e794145bc8b05b3763d0ed1a183
Cvbynathann <muhammadkidom@gmail.com>
2025-08-31 19:08:34 +0700

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ LOGGER_ID = int(getenv("LOGGER_ID", None))
 # Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", None))
 
-## Fill these variables if you're depRitiking on heroku.
+## Fill these variables if you're deploying on heroku.
 # Your heroku app name
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
@@ -32,15 +32,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TeamEspro/EsproMusicBot",
+    "https://github.com/WEREWOLF-DEMON/EsproMusicBotDemon",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "ritik")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/EsproUpdate")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/EsproSupport")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/INNOCENT_SONGS")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/INNOCENT_SONGS")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
